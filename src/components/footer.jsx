917c44411b51c8d6400004ea9143487857d6bd27
Abraham Pech <abraham.opp@gmail.com>
2023-02-20 19:21:42 -0500

    Setup inicial

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <main>
+      <h1>Este es mi Footer</h1>
+    </main>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <h1>Este es mi Header</h1>
+  )
+}
+
+export default Header
