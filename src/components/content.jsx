956ab891e651495c4e9b4485507f26b1e120b853
andres_realpozo <bollofrances@gmail.com>
2023-02-26 17:25:02 -0600

    :3

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -4,24 +4,25 @@ const Content = () => {
     <main>
       {/* Build your dream store*/}
       <div className="dream container ">
-        <h1 className="text-center uwu">Build your dream store</h1>
-        <p className="text-center uwu">A simple Webflow Ecommerce template to get you started.</p>
+        <h1 className="text-center font1">Build your dream store</h1>
+        <p className="text-center font2">A simple Webflow Ecommerce template to get you started.</p>
         
         <div className="mid">
-          <button className="btn btn-primary" type="button">Button</button>
+          <button className="btn btn-primary" type="button">Explore</button>
         </div>
 
       </div>
       {/*This is your space*/}
-    <div className="space container p-4">
-      <h2 className="uwu">This is your space</h2>
-      <h5 className="uwu">Talk about your business, your products, or yourself.</h5>
-      <div className="container">
+    <div className="container space">
+      <h2 className="Espacio ">This is your space</h2>
+      <p className="Espacio2 ">Talk about your business, your products, or yourself.</p>
+      <hr />
+      <div className="container Espacio3">
         <div className="row ">
-          <div className="col-5 uwu ">
+          <div className="col-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
           </div>
-          <div className="col-5 uwu">
+          <div className="col-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
           </div>
         </div>
@@ -31,33 +32,36 @@ const Content = () => {
 <br />
 
       {/*Featured Products*/}
-      <h2 className="text-center uwu">Featured Products</h2>
-      <p className="text-center uwu">Check out new and popular products</p>
+      <div className="container feat1">
+              <h2 className="text-center">Featured Products</h2>
+      <p className="text-center font3 ">Check out new and popular products</p>
+      </div>
+
       
       {/*Card section*/}
-      <div className="container">
+      <div className="test">
       <div className="row row-cols-1 row-cols-md-3 g-4">
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title downer">Card title</h5>
-              <p className="card-text downer">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title downer">awesomness</h5>
+              <p className="card-text downer font6">$50 usd</p>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card ">           
             <div className="card-body">
-              <h5 className="card-title downer">Card title</h5>
-              <p className="card-text downer">This card has supporting text below as a natural lead-in to additional content.</p>
+              <h5 className="card-title downer">directness</h5>
+              <p className="card-text downer font6">$ 60 usd</p>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title ">Card title</h5>
-              <p className="card-text text   ">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <h5 className="card-title ">simplicity</h5>
+              <p className="card-text font6">$40 usd</p>
             </div>
           </div>
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import Link from 'next/link';
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-                 <Link href="/"> 
-                    <h3 className="nav-link uwu">STORE</h3>
-                </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className="nav-item left">
-                <Link href="/about">
-                    <p className="nav-link">About</p>
-                </Link>
-              </li>
-            </ul>
-
-          </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid a">
+        <Link href="/" className='biger'> <h1 className="nav-link ">STORE</h1></Link>
+        <div className="collapse navbar-collapse ">
+          <ul className="navbar-nav der">
+            <li className="nav-item">
+              <Link href="/products" className='links'> <p className="nav-link">Products</p></Link>
+            </li>
+            <li className="nav-item">
+            <Link href="/about" className='links'> <p className="nav-link">About</p></Link>
+            </li>
+            <li className="nav-item">
+            <Link href="/contact" className='links'> <p className="nav-link">contact</p></Link>
+            </li>
+            <li className="nav-item boton1">
+              <Link href="/"> <button className='boton'> Cart</button></Link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
     );
 }
 
