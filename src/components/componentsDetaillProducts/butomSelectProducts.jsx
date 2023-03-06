d402ad4b54613f9a1f6f55bac7e390215f8b4323
jose ramon arias borges <jaborges666@gmail.com>
2023-03-05 22:37:45 -0500

    agregando mi css

--- src/components/componentsDetaillProducts/butomSelectProducts.jsx
+++ src/components/componentsDetaillProducts/butomSelectProducts.jsx
@@ -1,17 +1,17 @@
-
+import styles from '../../styles/butomSelectProducts.module.css';
 
 const ButomSelectProducts = () => {
     return (
         <div class="container">
         <div class="row">
             <div class="col">
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+                <input class="form-control form-control-lg" type="text" placeholder="" aria-label=".form-control-lg example" id={styles.SelectQuantity} />
             </div>
             <div class="col">
-                <button>Add to Cart</button>
+                <button className={styles.ButtonColor}>Add to Cart</button>
             </div>
             <div class="col">
-                <button>Buy now</button>
+                <button className={styles.ButtonColor}>Buy now</button>
             </div>
         </div>
     </div>
--- src/components/componentsDetaillProducts/headerProducts.jsx
+++ src/components/componentsDetaillProducts/headerProducts.jsx
@@ -1,12 +1,12 @@
-
+import styles from '../../styles/butomSelectProducts.module.css';
 
 const HeaderProducts = () => {
     return (
             <>
                 <h1>Unbelievable</h1>
-                <h1>$ 45.00 USD</h1>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.</p>
-                <hr>
+                <h1 className={styles.Letterh1}>$ 45.00 USD</h1>
+                <p className={styles.LetterP}>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.</p>
+                <hr className={styles.HrBlak}>
                 </hr>
             </>
     )
