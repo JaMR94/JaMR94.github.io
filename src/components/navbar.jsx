186accedeb5b4040609fba764a5a15f690d70e84
Pudin_con_pasas_UwU <71237346+Pudin-con-pasas-uwu@users.noreply.github.com>
2023-03-06 10:56:27 -0600

    Revert "Header/edgarperez7"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,56 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
 const Navbar = () => {
-  const [ menu ,setMenu ] = useState ( false)
-    const toggleMenu = () => {
-      setMenu ( !menu )
-    }
   return (
-
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid a">
         <Link href="/" className="biger">
           {" "}
-          <h1 className="Header-h1">STORE</h1>
+          <h1 className="nav-link ">STORE</h1>
         </Link>
-
-        <div className= {`nav ${ menu ? 'isActive':''}`}>
+        <div className="collapse navbar-collapse ">
           <ul className="navbar-nav der">
             <li className="nav-item">
               <Link href="/" className="links">
                 {" "}
-                <a className="nav-link">Products</a>
+                <p className="nav-link">Products</p>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="/" className="links">
                 {" "}
-                <a className="nav-link">About</a>
+                <p className="nav-link">About</p>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="/" className="links">
                 {" "}
-                <a className="nav-link">contact</a>
+                <p className="nav-link">contact</p>
               </Link>
-            </li> 
+            </li>
+            <li className="nav-item boton1">
+              <Link href="/">
+                {" "}
+                <button className="boton"> Cart</button>
+              </Link>
+            </li>
           </ul>
         </div>
-
-        <button>
-        <svg className="Header-svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-        </svg>
-       
-        </button>
-        <button 
-        onClick={toggleMenu}
-        className="Header-button">
-        <svg className="Header-svg1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-        </button>
-
       </div>
     </nav>
   );
