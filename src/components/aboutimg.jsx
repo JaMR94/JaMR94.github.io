6a268d5cdf2856c2fb8f0d0dccaeccb91dd7e571
kareng13 <71098347+kareng13@users.noreply.github.com>
2023-03-10 16:46:33 -0500

    Update aboutimg.jsx

--- src/components/aboutimg.jsx
+++ src/components/aboutimg.jsx
@@ -1,17 +1,6 @@
 const Aboutimg = () => {
     return (
       <main>
-<<<<<<< Updated upstream
-           <div className="container">
-      <div className="imgabout">
-      </div>
-      
-      </div>
-
-      <div className="container"> 
-      <div className="img2about">
-      </div>
-=======
         <div className="sectionabout">
       <div className="container" id="about-content">
             <div className="img">
@@ -19,7 +8,6 @@ const Aboutimg = () => {
             <div className="img2">
             </div>
           </div>
->>>>>>> Stashed changes
       </div>
       </main>
   );
