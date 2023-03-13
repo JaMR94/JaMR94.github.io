d161217ef9f92c4ee326339a3c32834a2fc03b73
Gerardo Casanova C <71162092+GerardoCasC@users.noreply.github.com>
2023-03-13 16:37:56 -0500

    Cambios CSS Y Layout
    Cambios en css + responsive y rceación de layout2

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,13 @@
 import { Inter } from '@next/font/google'
 import LoginPage from "@/components/LoginPage";
-import Layout from '@/components/Layouts/Layout'
+import Layout2 from '@/components/Layouts/Layout2'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-            <Layout>
+            <Layout2>
             <LoginPage/>
-            </Layout>
+            </Layout2>
             )
         }
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,13 +1,13 @@
 import { Inter } from '@next/font/google'
 import RegisterPage from "@/components/RegisterPage";
-import Layout from '@/components/Layouts/Layout'
+import Layout2 from '@/components/Layouts/Layout2'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-            <Layout>
+            <Layout2>
             <RegisterPage/>
-            </Layout>
+            </Layout2>
             )
         }
