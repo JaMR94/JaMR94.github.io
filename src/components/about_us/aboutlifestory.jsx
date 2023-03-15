a6fbea6a5ee358b9a237e1caf168c99f2f16f3b2
Shadnucks <123423856+Shadnucks@users.noreply.github.com>
2023-03-15 00:33:10 -0500

    Update aboutlifestory.jsx

--- src/components/about_us/aboutlifestory.jsx
+++ src/components/about_us/aboutlifestory.jsx
@@ -4,20 +4,21 @@ const Aboutlifestory = () => {
         <div className="sectionabout">
          <div className="container" id="about-content">
           <div className="abouttitle2">
-            <h1>This is our life story</h1>
-          <p>Beginning of tea parties</p>
+            <h1>Esta es nuestra historia</h1>
+          <p>El comienzo de nuestra compañía</p>
           </div>
           </div>
           <div className="divider "></div>
           <div className="container">
         <div className="row justify-content-center aboutcontext">
           <div className="col-sm-6 col-md-5 abouttext">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. 
-          Nunc ut sem vitae risus tristique posuere.
+          Somos una compañía que ofrece productos de buena calidad a un precio razonable mediante compras en línea. Nosotros le ofrecemos un servicio de entrega de calidad, por lo cual puede asegurarse de que su envió llegara en tiempo y forma, y si se le presenta un problema o duda respecto a nuestros productos y servicios puede comunicarse con nuestro rápido y responsivo servicio al cliente.
           </div>
           <div className="col-sm-6 col-md-5 abouttext">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. 
-          Nunc ut sem vitae risus tristique posuere.
+          Opiniones
+          Pr_Goomez: Mi compra llego a mi chaletito en el tiempo esperado y sin ningún problema con el paquete. 4.5/5
+          Giuseppe133: Ofrecen unos productos de una buenísima calidad, aunque el tiempo de espera de tu pedido puede ser algo largo dependiendo de donde vivas. 3.5/5
+
           </div>
           </div>
           </div>
