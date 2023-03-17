059e7eb066ab0144600abaa1571cf7f56283e06f
andres_realpozo <bollofrances@gmail.com>
2023-03-17 15:22:17 -0600

    Extras & Hotfix
    se cambio el favicon de la pagina
    se cambio el title del header
    se cambio unas cosillas en el inicio

--- src/components/homeblog/space.jsx
+++ src/components/homeblog/space.jsx
@@ -4,28 +4,24 @@ const Space = () => {
   return (
     <div>
       <div className="container space">
-        <h2 className="Espacio ">This is your space</h2>
+        <h2 className="Espacio ">Esta es tu tienda</h2>
         <p className="Espacio2 ">
-          Talk about your business, your products, or yourself.
+        Porque comprar en otras tiendas, cuando puedes comprar en esta ;)
         </p>
         <hr />
         <div className="container Espacio3">
           <div className="row ">
-            <div className="col-lg-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique. Duis cursus,
-              mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
-              libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum
-              lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+            <div className="col-lg-6">
+              <p>  Comprar en nuestra tienda online es la mejor opción para ti, porque te ofrecemos una amplia selección de productos de alta calidad a precios competitivos. Además, nuestro proceso de apartados es fácil, rápido y seguro. 
+            Nos esforzamos por brindarte una experiencia de compra excepcional y un servicio al cliente amigable y profesional. ¡No esperes más y haz tu compra con nosotros hoy mismo!</p>
               <br />
               <br />
             </div>
-            <div className="col-lg-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique. Duis cursus,
-              mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
-              libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum
-              lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+            <div className="col-lg-6">
+              <p>Me podría matar poniendo texto default en esta parte, pero honestamente ocupo rellenar este espacio, 
+                por favor compra en nuestra tienda, pronto Amazon o mercado libre se quedarán detrás de nosotros,
+                nuestro equipo de desarrollo, programadores y vendedores te lo agradecerían mucho.
+              </p>
             </div>
           </div>
         </div>
