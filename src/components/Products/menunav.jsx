6fdb9b3f50216213e2c3addc01335eaf158b80c6
jose ramon arias borges <jaborges666@gmail.com>
2023-03-17 03:55:45 -0400

    Update menunav.jsx

--- src/components/Products/menunav.jsx
+++ src/components/Products/menunav.jsx
@@ -1,76 +1,27 @@
-import * as React from 'react';
-import { Box } from '@mui/system';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import MenuItem from '@mui/material/MenuItem';
-import Divider from '@mui/material/Divider';
+import Link from "next/link"; 
+import styles from '../../styles/butomSelectProducts.module.css';
+import React from 'react';
 
-const Menunav = () => {
-const [value, setValue] = React.useState('recents');
-const handleChange = (event, newValue) => {
-setValue(newValue); };
-const [anchorEl, setAnchorEl] = React.useState(null);
-const open = Boolean(anchorEl);
-const handleClick = (event) => {
-  setAnchorEl(event.currentTarget);
-};
-const handleClose = () => {
-  setAnchorEl(null);
-};
+const menunav = (props) => {
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        
-        <BottomNavigationAction label="Inicio"/>
-        <div>
-            <BottomNavigationAction id="figurasmenu" label="Figuras"
-            MenuListProps={{
-            'aria-labelledby': 'demo-customized-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            >
-                <MenuItem onClick={handleClose} disableRipple>
-                Edit
-                </MenuItem>
-                <MenuItem onClick={handleClose} disableRipple>
-                Duplicate
-                </MenuItem>
-                <Divider sx={{ my: 0.5 }} />
-                <MenuItem onClick={handleClose} disableRipple>
-                Archive
-                </MenuItem>
-                <MenuItem onClick={handleClose} disableRipple>
-                More
-                </MenuItem>
-            </BottomNavigationAction>
-        </div>
-
-
-        <BottomNavigationAction label="Mangas" />
-
-
-        <BottomNavigationAction label="Apparel" />
-
-
-        <BottomNavigationAction label="Preventas" />
-        <BottomNavigationAction label="Juegos" />
-        <BottomNavigationAction label="Otros" />
-
-
-        <BottomNavigationAction label="Series" />
-
-
-        <BottomNavigationAction label="Rebajas" />
-      </BottomNavigation>
-    </Box>
+    
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#" id={styles.linksDelMenuNav}>Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id={styles.linksDelMenuNav}>Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id={styles.linksDelMenuNav}>Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id={styles.linksDelMenuNav}>Link</a>
+      </li>
+    </ul>
   );
-}
-export default Menunav
+};
+
+
+
+export default menunav; // Exportamos el componente Cardsection
